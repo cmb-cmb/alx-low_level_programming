6587169e910b96e4345bcdaa53828cf2e81002c8
cmb-cmb <belindahchangalwa@gmail.com>
2023-08-19 01:19:17 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,8 +4,9 @@
  * @array: the array of integers
  * @size: the number of elements in the array
  * @cmp: pointer to the function to be used to compare values
- * Return: If no element matches or size <= 0, -1 
- *		Otherwise, the index of the first element for which the cmp function does not return 0
+ * Return: If no element matches or size <= 0, -1
+ *		Otherwise, the index of the first element
+ *		for which the cmp function does not return 0
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
